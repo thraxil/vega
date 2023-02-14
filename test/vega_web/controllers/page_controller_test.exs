52d511b663b00de13b0cf83087ad9403a9ccaccb
Anders Pearson <anders@thraxil.org>
2023-02-14 21:49:09 +0000

    clean up breadcrumbs a bit

--- test/vega_web/controllers/page_controller_test.exs
+++ test/vega_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule VegaWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Thraxil"
+    assert html_response(conn, 200) =~ "thraxil.org"
   end
 end
