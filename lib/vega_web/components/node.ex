60c2beb361255f45980617b240c192083674f406
Anders Pearson <anders@thraxil.org>
2023-06-15 22:05:39 +0100

    more heex warnings

--- lib/vega_web/components/node.ex
+++ lib/vega_web/components/node.ex
@@ -7,15 +7,15 @@ defmodule VegaWeb.Components.Node do
   import Phoenix.HTML.{Link, Tag}
   import Phoenix.HTML
 
-  def node_link(%{node: node} = assigns) do
+  def node_link(assigns) do
     ~H"""
-    <.link navigate={VegaWeb.PageView.node_path(node)} class="text-slate-900"><%= node.title %></.link>
+    <.link navigate={VegaWeb.PageView.node_path(@node)} class="text-slate-900"><%= @node.title %></.link>
     """
   end
 
-  def node_title(%{node: node} = assigns) do
+  def node_title(assigns) do
     ~H"""
-    <h2 class="bg-slate-300 my-0 "><%= link(node.title, to: VegaWeb.PageView.node_path(node),
+    <h2 class="bg-slate-300 my-0 "><%= link(@node.title, to: VegaWeb.PageView.node_path(@node),
     class: "no-underline text-slate-900 ") %></h2>
     """
   end
@@ -49,27 +49,27 @@ defmodule VegaWeb.Components.Node do
     |> List.to_string()
   end
 
-  def byline(%{node: node} = assigns) do
+  def byline(assigns) do
     ~H"""
-    <p class="bg-slate-100 my-0">By <%= link(node.user.fullname, to:
-    Routes.page_path(VegaWeb.Endpoint, :user_detail, node.user.username),
-    class: "text-slate-900") %> <%= dformat_node(node.created) %></p>
+    <p class="bg-slate-100 my-0">By <%= link(@node.user.fullname, to:
+    Routes.page_path(VegaWeb.Endpoint, :user_detail, @node.user.username),
+    class: "text-slate-900") %> <%= dformat_node(@node.created) %></p>
     """
   end
 
-  def inline_byline(%{node: node} = assigns) do
+  def inline_byline(assigns) do
     ~H"""
-    By <%= link(node.user.fullname, to:
-    Routes.page_path(VegaWeb.Endpoint, :user_detail, node.user.username),
-    class: "text-slate-900") %> <%= dformat_node(node.created) %>
+    By <%= link(@node.user.fullname, to:
+    Routes.page_path(VegaWeb.Endpoint, :user_detail, @node.user.username),
+    class: "text-slate-900") %> <%= dformat_node(@node.created) %>
     """
   end
 
-  def node_tags(%{node: node} = assigns) do
+  def node_tags(assigns) do
     ~H"""
-    <%= if length(node.tags) > 0 do %>
+    <%= if length(@node.tags) > 0 do %>
     <p>Tags: 
-    <%= for tag <- node.tags do %>
+    <%= for tag <- @node.tags do %>
     <%= link(tag.name, to: Routes.page_path(VegaWeb.Endpoint, :tag_detail, tag.slug), class: "badge") %>
     <% end %>
     </p>
@@ -77,28 +77,28 @@ defmodule VegaWeb.Components.Node do
     """
   end
 
-  def node_content(%{node: %{type: "post"}, content: content} = assigns) do
+  def node_content(%{node: %{type: "post"}, content: _content} = assigns) do
     ~H"""
-    <%= raw content.body_html %>
+    <%= raw @content.body_html %>
     """
   end
 
-  def node_content(%{node: %{type: "image"} = node, content: content} = assigns) do
+  def node_content(%{node: %{type: "image"} = _node, content: _content} = assigns) do
     ~H"""
-    <%= if content.rhash && content.ext do %>
+    <%= if @content.rhash && @content.ext do %>
     <%= img_tag("https://d2f33fmhbh7cs9.cloudfront.net/image/" <>
-    content.rhash <> "/960w/" <> to_string(node.id) <> "." <>
-    content.ext,
-    title: node.title
+    @content.rhash <> "/960w/" <> to_string(@node.id) <> "." <>
+    @content.ext,
+    title: @node.title
     ) %>
     <% else %>
     <p>[missing]</p>
     <% end %>
-    <%= raw Earmark.as_html!(content.description) %>
+    <%= raw Earmark.as_html!(@content.description) %>
     """
   end
 
-  def node_content(%{node: %{type: "bookmark"} = _node, content: content} = assigns) do
+  def node_content(%{node: %{type: "bookmark"} = _node, content: _content} = assigns) do
     ~H"""
     <p><.link navigate={@content.url}><%= @node.title %></.link></p>
     <%= raw Earmark.as_html!(@content.description) %>
