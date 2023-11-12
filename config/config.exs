8141a0b15c2f3343fabc0d802a68a9b1fb804f85
Anders Pearson <anders@thraxil.org>
2023-11-12 15:50:20 +0000

    :arrow_up: sentry 9.1.0

--- config/config.exs
+++ config/config.exs
@@ -63,7 +63,7 @@ config :sentry,
   dsn: System.get_env("SENTRY_DSN"),
   environment_name: Mix.env(),
   enable_source_code_context: true,
-  root_source_code_path: File.cwd!(),
+  root_source_code_paths: [File.cwd!()],
   tags: %{
     env: "production"
   },
